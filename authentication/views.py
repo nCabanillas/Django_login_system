e20eed21123fb8878db4811dffe07c94037bf85f
Noe Cabanillas <73083315+nCabanillas@users.noreply.github.com>
2024-02-06 14:50:06 -0500

    login_app:fix

--- authentication/views.py
+++ authentication/views.py
@@ -7,7 +7,7 @@ from geeksforgeeks import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth import authenticate, login, logout
 from . tokens import generate_token
 
@@ -86,7 +86,7 @@ def signup(request):
 
 def activate(request,uidb64,token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         myuser = User.objects.get(pk=uid)
     except (TypeError,ValueError,OverflowError,User.DoesNotExist):
         myuser = None
